39fa3a23b1dd240ade91b7a02e724a647e385c98
Will Milton <wa.milton@gmail.com>
2015-08-03 09:45:08 -0400

    add -f flag to ln

--- linkin.sh
+++ linkin.sh
@@ -6,5 +6,5 @@ pushd $(dirname $0)
 dir=$PWD
 for file in `ls ${dir}/*.bash`
 do
-  ln -s "${file}" "$HOME/.bash_it/custom/$(basename $file)"
+  ln -sf "${file}" "$HOME/.bash_it/custom/$(basename $file)"
 done
